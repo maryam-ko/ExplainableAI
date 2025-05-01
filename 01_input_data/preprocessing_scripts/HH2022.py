6770f60daa96afef1f136e99ef2ad97ee0be74f5
maryam-ko <maryamkoddus@gmail.com>
2025-05-01 22:03:09 +0100

    updated files

--- 01_input_data/preprocessing_scripts/HH2022.py
+++ 01_input_data/preprocessing_scripts/HH2022.py
@@ -8,7 +8,7 @@ import re
 dataset = 'HH2022'
 
 print('Loading raw data for', dataset, '...')
-data = pd.read_csv('/Users/maryamkoddus/Documents/maryam-ko-QMUL-MSc-Project/01_input_data/raw_data/41598_2022_8430_MOESM12_ESM.csv', header=0)
+data = pd.read_csv('/data/home/bt24990/maryam-ko-QMUL-MSc-Project/01_input_data/raw_data/41598_2022_8430_MOESM12_ESM.csv', header=0)
 print('Raw data loaded.')
 data
 
@@ -61,7 +61,7 @@ def find_position_in_gene(dataset, seq_column, fasta_path):
     dataset['StartPosition'] = dataset['Gene Names'].map(positions_dict)
     return dataset
 
-fasta_path = '/Users/maryamkoddus/Documents/maryam-ko-QMUL-MSc-Project/01_input_data/raw_data/UP000005640_9606.fasta'
+fasta_path = '/data/home/bt24990/maryam-ko-QMUL-MSc-Project/01_input_data/raw_data/UP000005640_9606.fasta'
 data = find_position_in_gene(data, 'Sequence', fasta_path)
 
 # Print the data with start positions
@@ -96,7 +96,7 @@ def match_seq_to_genename(dataset, seq_column):
     dataset: <pd.Dataframe> with an additional column containing gene names
     '''    
 
-    fasta_sequence = list(SeqIO.parse(open(f'/Users/maryamkoddus/Documents/maryam-ko-QMUL-MSc-Project/01_input_data/raw_data/UP000005640_9606.fasta'), "fasta"))
+    fasta_sequence = list(SeqIO.parse(open(f'/data/home/bt24990/maryam-ko-QMUL-MSc-Project/01_input_data/raw_data/UP000005640_9606.fasta'), "fasta"))
     
     
     gene_dict = {}
@@ -214,7 +214,7 @@ data = clean_phosID_col(data)
 print("After cleaning phosphosite_ID column:")
 data
 
-data.to_csv(f'/Users/maryamkoddus/Documents/maryam-ko-QMUL-MSc-Project/01_input_data/PreprocessedDatasets/HH2022.csv', index=False)
+data.to_csv(f'/data/home/bt24990/maryam-ko-QMUL-MSc-Project/01_input_data/PreprocessedDatasets/HH2022.csv', index=False)
 
 
 print(dataset, 'has been saved to CSV successfully!', data)
---
+++ MV2014.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#$ -cwd              # Set the working directory for the job to the current directory
+#$ -pe smp 4         # Request 8 cores (adjust based on your needs)
+#$ -l h_rt=2:0:0     # Request 2 hours of runtime (adjust as needed)
+#$ -l h_vmem=12G     # Request 12 GB of RAM
+#$ -o MV2014.csv
+#$ -e MV2014_error.txt
+
+# Load the Python module
+module load python/3.11.7-gcc-12.2.0
+
+# Activate your virtual environment
+source ~/myenv/bin/activate
+
+python /data/home/bt24990/maryam-ko-QMUL-MSc-Project/01_input_data/preprocessing_scripts/MV2014.py
