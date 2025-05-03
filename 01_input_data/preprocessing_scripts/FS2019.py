2beb3402690689de36fa85ad3b240cf8bfc70a8d
maryam-ko <maryamkoddus@gmail.com>
2025-05-03 02:05:23 +0100

    fs2019 scripts

--- 01_input_data/preprocessing_scripts/FS2019.py
+++ 01_input_data/preprocessing_scripts/FS2019.py
@@ -19,7 +19,6 @@ data = data[data['Localization prob'] >= 0.85]
 
 # Filtering out semi-colons from 'Amino acid', 'Positions within proteins', and 'Gene names' columns
 data = data[~data['Amino acid'].str.contains(';', na=False)]
-data = data[~data['Position'].str.contains(';', na=False)]
 data = data[~data['Gene names'].str.contains(';', na=False)]
 data
 
