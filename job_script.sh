c6a9fbaa3e6071da0fa1dc6dedd13e10672b4891
maryam-ko <maryamkoddus@gmail.com>
2025-05-03 20:15:33 +0100

    job script

--- job_script.sh
+++ job_script.sh
@@ -3,13 +3,13 @@
 #$ -pe smp 4         
 #$ -l h_rt=1:0:0     
 #$ -l h_vmem=12G     
-#$ -o FS2019.csv     
-#$ -e FS2019_error.txt  
+#$ -o GRW2016.csv     
+#$ -e GRW2016_error.txt  
 #$ -M m.koddus@se24.qmul.ac.uk   
 #$ -m e                        
 
 module load python/3.11.7-gcc-12.2.0
 source ~/myenv/bin/activate
 
-python /data/home/bt24990/maryam-ko-QMUL-MSc-Project/01_input_data/preprocessing_scripts/FS2019.py
+python /data/home/bt24990/maryam-ko-QMUL-MSc-Project/01_input_data/preprocessing_scripts/GRW2016.py
  
